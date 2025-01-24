697f97154a85f3201b550476425023ab14a2ac77
Dan Welling <dwelling@umich.edu>
2025-01-24 16:48:08 -0500

    Updated for correct calculation of diffuse aurora.

--- src/ModMagnit.f90
+++ src/ModMagnit.f90
@@ -23,24 +23,11 @@ module ModMagnit
 
   ! Loss cone factors set flux in loss cones for proton & electron diffuse,
   ! monoenergetic, and broadband precip. Factors are for Energy Flux and
-  ! Number Flux, each.
+  ! Number Flux, each. See Mukhopadhyay et al. 2022 for details.
   real :: ConeEfluxDife = 0.217, ConeEfluxDifp = 0.207, &
           ConeEfluxMono = 1.000, ConeEfluxBbnd = 2.247, &
           ConeNfluxDife = 0.055, ConeNfluxDifp = 0.038, &
           ConeNfluxMono = 0.741, ConeNfluxBbnd = 0.494
-
-!  MinWidth = 5.0 * cPi / 180.0
-!  nHalfSmooth = 5
-!     MulFac_Dae = 1.0e22
-!     MulFac_Def = 5.0e19
-!     MulFac_ef = 0.2e7
-!     MulFac_ae = 1.0 / 1.0e11
-!  MulFac_Dae = 1.0e22
-!  MulFac_Def = 1.0e19
-!  MulFac_ef = 0.3e6! * 1e03
-!  MulFac_ae = 3.65e-12
-
-  ! reminder: GmRhoFloor = 1E-21, GmPFloor = 1E-13
 
   contains
   !============================================================================
@@ -75,20 +62,21 @@ module ModMagnit
 
     ! Given magnetospheric density, pressure, and FACs, calculate diffuse and
     ! monoenergetic precipitating fluxes.
+    ! Resulting units are W/m2 and KeV for energy flux
+    ! and average energy, respectively.
 
     character(len=*), intent(in) :: NameHemiIn
 
     ! Set arrays to hold precip values. Magnetospheric pressure and density
     ! from GM (SI units), Average Energy and Energy Flux (units of KeV and
-    ! mW/m2 aka ergs/cm2, respectively) for each precip type.
+    ! W/m2, respectively) for each precip type.
     real, intent(out), dimension(IONO_nTheta, IONO_nPsi) :: &
         AvgEDiffe_II, AvgEDiffi_II, AvgEMono_II, AvgEBbnd_II, &
         EfluxDiffe_II, EfluxDiffi_II, EfluxMono_II, EfluxBbnd_II
 
     ! Set arrays to hold magnetospheric values.
     real, dimension(IONO_nTheta, IONO_nPsi) :: &
-        MagP_II, MagNp_II, MagPe_II, MagNe_II, MagRho_II, &
-        NfluxDiffe_II, NfluxDiffi_II
+        MagP_II, MagNp_II, MagPe_II, MagNe_II, NfluxDiffe_II, NfluxDiffi_II
 
     integer :: j
 
@@ -106,44 +94,45 @@ module ModMagnit
         MagNp_II = iono_north_rho / cProtonMass
     else if (NameHemiIn == 'south')then
         MagP_II = iono_south_p
-        MagRho_II = iono_south_rho / cProtonMass
+        MagNp_II = iono_south_rho / cProtonMass
     else
       call CON_stop(NameSub//' : unrecognized hemisphere - '//NameHemiIn)
     end if
 
-    ! Set default/background values.
-
     ! If we need OCFLB calculation, we would put it here.
 
     ! If not using explicit Pe from GM, obtain values from proton values.
-    ! flip across noon midnight, etc. to fill MagPe, MagRhoe
+    ! Flip across noon midnight, etc. to fill MagPe, MagNe
     ! Scale Pe using ratioPe
     if(.not. DoUseGmPe) then
       do j=1, Iono_nPsi
         MagPe_II(:, j) = ratioPe * MagP_II(:, IONO_nPsi-j+1)
         MagNe_II(:, j) = MagNp_II(:, IONO_nPsi-j+1)
       end do
+    else
+      ! Placeholder for electron entropy coupling.
+      MagNe_II = MagNp_II
     end if
 
     ! Calculate diffuse precipitation: protons.
-    AvgEDiffi_II  = MagP_II / (MagNp_II * cKEV)  ! T = P/nk in keV
-    NfluxDiffi_II = ConeNfluxDifp * MagNp_II * (cKEV * AvgEDiffi_II)**0.5 / &
-                    sqrt(2 * cPi * cProtonMass)  ! units of mW/m2 or ergs/cm2
-    ! units of mW/m2 or ergs/cm2
-    EfluxDiffi_II = 2000 * ConeEfluxDifp * MagNp_II * &
-                    (cKEV * AvgEDiffi_II)**1.5 / sqrt(2 * cPi * cProtonMass)
-    ! Recalc to make consistent with ConeFactors
-    AvgEDiffi_II = EfluxDiffi_II / NfluxDiffi_II
+    AvgEDiffi_II  = MagP_II / MagNp_II  ! Temp = P/nk in Joules
+    NfluxDiffi_II = ConeNfluxDifp * MagNp_II * AvgEDiffi_II**0.5 / &
+                    sqrt(2 * cPi * cProtonMass)  ! units of #/m2/s
+    ! units of W/m2
+    EfluxDiffi_II = 2 * ConeEfluxDifp * MagNp_II * &
+                    AvgEDiffi_II**1.5 / sqrt(2 * cPi * cProtonMass)
+    ! Recalc to make consistent with ConeFactors (and get units of keV)
+    AvgEDiffi_II = EfluxDiffi_II / (NfluxDiffi_II * cKEV)
 
     ! Calculate diffuse precipitation: electrons.
-    AvgEDiffe_II  = MagPe_II / (MagNe_II * cKEV)  ! T = P/nk in keV
-    NfluxDiffe_II = ConeNfluxDife * MagNe_II * (cKEV * AvgEDiffe_II)**0.5 / &
-                 sqrt(2 * cPi * cElectronMass)  ! units of mW/m2 or ergs/cm2
-    ! units of mW/m2 or ergs/cm2
-    EfluxDiffe_II = 2000 * ConeEfluxDife * MagNe_II * &
-                    (cKEV * AvgEDiffe_II)**1.5 / sqrt(2 * cPi * cElectronMass)
-    ! Recalc to make consistent with ConeFactors
-    AvgEDiffe_II = EfluxDiffe_II / NfluxDiffe_II
+    AvgEDiffe_II  = MagPe_II / MagNe_II  ! T = P/nk in Joules
+    NfluxDiffe_II = ConeNfluxDife * MagNe_II * AvgEDiffe_II**0.5 / &
+                 sqrt(2 * cPi * cElectronMass)  ! units of #/m2/s
+    ! units of W/m2
+    EfluxDiffe_II = 2 * ConeEfluxDife * MagNe_II * &
+                    AvgEDiffe_II**1.5 / sqrt(2 * cPi * cElectronMass)
+    ! Recalc to make consistent with ConeFactors (and get units of keV)
+    AvgEDiffe_II = EfluxDiffe_II / (NfluxDiffe_II * cKEV)
 
   end subroutine magnit_gen_fluxes
   !============================================================================
